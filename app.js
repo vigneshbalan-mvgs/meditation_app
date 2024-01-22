1c61170f6b3cc92aa4d6aa23a28493f6d16aa42c
vigneshbalan-mvgs <vigneshbalanmvgs2003@gmail.com>
2024-01-22 21:53:08 +0530

    added some js in app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+const circleProgress = document.querySelector('.circle-progress');
+const numberOfBreaths = document.querySelector('.breath-input');
+const start = document.querySelector('.start');
+const instructions = document.querySelector('.instructions');
+const breathsText = document.querySelector('.breaths-text');
+let breathsLeft = 3;
+
 
